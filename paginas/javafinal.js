9eac39b345ee7ae83629c8b638f6156ac207fcda
Romina Campora <romicampora@gmail.com>
2022-09-30 13:58:20 -0300

    first commit

---
+++ paginas/javafinal.js
@@ -0,0 +1,18 @@
+
+
+
+let edad = prompt("Cuantos años tenes?");
+
+if (edad < 18) {
+    alert("Por favor recorra el sitio con responsabilidad");
+  } else {
+    alert("Bienvenidx a nuestro local!");
+  };
+
+
+ window.confirm(message);
+    if (window.confirm("Seguro queres retirarte?")) {
+    window.open("exit.html", "Muchas gracias por pasar!");
+    };
+
+
