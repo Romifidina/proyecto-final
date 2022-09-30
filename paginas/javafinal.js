bf2acff63c29bed2b0c46d36378e57ca5d35b595
Romina Campora <romicampora@gmail.com>
2022-09-30 14:53:04 -0300

    correccion de links

--- paginas/javafinal.js
+++ paginas/javafinal.js
@@ -1,7 +1,7 @@
 
 
 
-let edad = prompt("Cuantos años tenes?");
+//let edad = prompt("Cuantos años tenes?");
 
 if (edad < 18) {
     alert("Por favor recorra el sitio con responsabilidad");
@@ -9,7 +9,11 @@ if (edad < 18) {
     alert("Bienvenidx a nuestro local!");
   };
 
+  //si ya hay respuesta no volver a preguntar, de lo contrario preguntar hasta que haya respuesta
+  window.localStorage(edad);
+    if (localStorage)
 
+// consultar antes de cerrar la pagnia si quiere salir del sitio
  window.confirm(message);
     if (window.confirm("Seguro queres retirarte?")) {
     window.open("exit.html", "Muchas gracias por pasar!");
