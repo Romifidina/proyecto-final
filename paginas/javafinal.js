2be9050d50e88a8daf99aa7221e68080364d8ac6
Romina Campora <romicampora@gmail.com>
2022-10-01 02:01:30 -0300

    pop up

--- paginas/javafinal.js
+++ paginas/javafinal.js
@@ -1,21 +1,33 @@
 
 
-let edad = localStorage.getItem('edad');
-if(!edad){
-    edad = prompt("Cuantos años tenes?");
-    localStorage.setItem('edad', edad);
+//let edad = localStorage.getItem('edad');
+//if(!edad){
+    //edad = prompt("Cuantos años tenes?");
+    //localStorage.setItem('edad', edad);
+//}
+//let alert = localStorage.getItem('alert');
+//if(!alert){
+     //if (edad < 18) {
+      //alert("Por favor recorra el sitio con responsabilidad");
+    //} else {
+      //alert("Bienvenidx a nuestro local!");
+    //};
+	//alert = true;
+    //localStorage.setItem('alert', alert);
+//}
+let popUp = localStorage.getItem('respuesta');
+
+if (!popUp) {
+  respuesta = prompt('Cuantos años tenes?');
+
+  if (respuesta < 18) {
+    alert('Por favor recorra el sitio con responsabilidad');
+  } else {
+    alert('Bienvenidx a nuestro local!');
+  }
+
+  localStorage.setItem('respuesta', respuesta);
 }
-let alert = localStorage.getItem('alert');
-if(!alert){
-     if (edad < 18) {
-      alert("Por favor recorra el sitio con responsabilidad");
-    } else {
-      alert("Bienvenidx a nuestro local!");
-    };
-	alert = true;
-    localStorage.setItem('alert', alert);
-}
-  
 
 // consultar antes de cerrar la pagnia si quiere salir del sitio
 // window.confirm(message);
