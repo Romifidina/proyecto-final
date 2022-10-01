467e0344f6e3b83551451132d8c7a4fa2c74820a
Romina Campora <romicampora@gmail.com>
2022-10-01 01:39:07 -0300

    java

--- paginas/javafinal.js
+++ paginas/javafinal.js
@@ -1,27 +1,34 @@
 
 
-let edad = prompt("Cuantos años tenes?");
-
-if (edad < 18) {
-    alert("Por favor recorra el sitio con responsabilidad");
-  } else {
-    alert("Bienvenidx a nuestro local!");
-  };
-  
-  //si ya hay respuesta no volver a preguntar, de lo contrario preguntar hasta que haya respuesta
-  if( !localStorage.getItem(edad) ){
-
-    document.getElementById('mensaje').style.display= 'block';
-    localStorage.setItem(edad,1); 
-
-} else {
-    document.getElementById('mensaje').style.display= 'none';
+let edad = localStorage.getItem('edad');
+if(!edad){
+    edad = prompt("Cuantos años tenes?");
+    localStorage.setItem('edad', edad);
 }
+let alert = localStorage.getItem('alert');
+if(!alert){
+     if (edad < 18) {
+      alert("Por favor recorra el sitio con responsabilidad");
+    } else {
+      alert("Bienvenidx a nuestro local!");
+    };
+	alert = true;
+    localStorage.setItem('alert', alert);
+}
+  
 
 // consultar antes de cerrar la pagnia si quiere salir del sitio
- window.confirm(message);
-    if (window.confirm("Seguro queres retirarte?")) {
-    window.open("exit.html", "Muchas gracias por pasar!");
-    };
+// window.confirm(message);
+   // if (window.confirm("Seguro queres retirarte?")) {
+    //window.open("exit.html", "Muchas gracias por pasar!");
+    //};
+  //si ya hay respuesta no volver a preguntar, de lo contrario preguntar hasta que haya respuesta
+ // if( !localStorage.getItem(edad) ){
 
+    //document.getElementById('mensaje').style.display= 'block';
+    //localStorage.setItem(edad,1); 
+
+//}else {
+    //document.getElementById('mensaje').style.display= 'none';
+//}
 
